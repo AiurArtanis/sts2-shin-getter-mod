15892d8ff4a0e4ab4b7ccbb07aa48a552862b50c
Artanis <98091664+AiurArtanis@users.noreply.github.com>
2026-07-20 22:48:56 +0800

    fix: resolve combat animation regressions

--- shin-getter-mod-godot/src/Patches/ShinGetterCardPlayVoicePatch.cs
+++ shin-getter-mod-godot/src/Patches/ShinGetterCardPlayVoicePatch.cs
@@ -1,6 +1,12 @@
+using System.Linq;
 using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using ShinGetterMod.Audio;
+using ShinGetterMod.Models.Cards;
+using ShinGetterMod.Models.Characters;
+using ShinGetterMod.Nodes.Combat;
 
 namespace ShinGetterMod.Patches;
 
@@ -10,5 +16,21 @@ internal static class ShinGetterCardPlayVoicePatch
     private static void Prefix(CardModel __instance)
     {
         ShinGetterVoiceService.TryPlayCardVoiceAtCardPlayStart(__instance);
+
+        if (__instance is ShinGetterCardBase || __instance.Owner?.Character is not ShinGetter)
+            return;
+
+        string trigger = __instance.Type switch
+        {
+            CardType.Attack => "Attack",
+            CardType.Skill when HasBlock(__instance) => "Block",
+            CardType.Skill or CardType.Power => "Cast",
+            _ => "Dash",
+        };
+
+        NShinGetterStaticVisuals.TryPlayCreatureActionAnimation(__instance.Owner.Creature, trigger);
     }
+
+    private static bool HasBlock(CardModel card) =>
+        card.DynamicVars.Values.Any(variable => variable is BlockVar or CalculatedBlockVar);
 }
